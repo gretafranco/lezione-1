0738664f8eb89586e5abc47f1f4c9bafa419b9f3
gretafranco <greta.franco@mail.polimi.it>
2026-03-25 17:28:42 +0100

    add state and derived property

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script>
     console.log('Hello from the page component!');
+    let count = $state(2);
+    let doubled = $derived(count * 2);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button on:click={() => count++}>
+    clicks:{count}
+</button>
+
+<span>doubled is {doubled}</span>
 
 <style>
 h1 {
