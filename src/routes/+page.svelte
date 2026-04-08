828dcb36a958067b74afc66f2daeebb2214f17ae
gretafranco <greta.franco@mail.polimi.it>
2026-04-08 15:00:30 +0200

    add script tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
+<script>console.log('Hello, Svelte!');</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
