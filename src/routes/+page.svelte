b398f23ea67c777bc98a29de29b24a2ffddf81f5
gretafranco <greta.franco@mail.polimi.it>
2026-04-08 15:24:45 +0200

    make properties derived

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   let count = $state(0)
+  let doubled = $derived(count * 2)
 
   console.log('Hello, Svelte!')
 </script>
@@ -11,9 +12,10 @@
   clicks:{count}
 </button>
 
+<span>doubled is {doubled}</span>
+
 <style>
   h1 {
     color: deepskyblue;
   }
-  
 </style>
